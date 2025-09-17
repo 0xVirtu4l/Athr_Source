c0c70cfa41b04259f31f8c27c3cf2d737f854a9f
mayman007 <medoems2210@gmail.com>
2025-09-18 01:45:13 +0300

    changed proxy blocker port

--- Web-APIs/proxy-blocker/main.py
+++ Web-APIs/proxy-blocker/main.py
@@ -97,4 +97,4 @@ async def get_ip_info(
             return {"access_granted": True, "warning": "IP check service unavailable."}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
