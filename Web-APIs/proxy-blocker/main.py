83aff70f21e3087113e6177def61e9badc9dd877
mayman007 <medoems2210@gmail.com>
2025-09-18 02:57:27 +0300

    web-apis: fixed a bug

--- Web-APIs/proxy-blocker/main.py
+++ Web-APIs/proxy-blocker/main.py
@@ -9,6 +9,8 @@ from firebase_admin import auth
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # --- CONFIGURATION ---
