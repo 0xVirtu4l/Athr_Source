225c7923d2a1cfb47a5a00e1798974de56eee976
mayman007 <medoems2210@gmail.com>
2025-09-18 02:40:00 +0300

    web-apis: added origins

--- Web-APIs/proxy-blocker/main.py
+++ Web-APIs/proxy-blocker/main.py
@@ -25,7 +25,9 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://127.0.0.1:8080",
     "http://localhost:5500",
+    "http://127.0.0.1:5500",
     "https://athr-78dc5.web.app",
     "https://athr.pages.dev",
     "https://athr.mohamedayman.org",
