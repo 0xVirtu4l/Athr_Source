a4a962ba3faa7f3b916b0fbcde62041b073cb76f
mayman007 <medoems2210@gmail.com>
2025-09-29 03:02:01 +0300

    web-apis: updated port

--- Web-APIs/dashboard/main.py
+++ Web-APIs/dashboard/main.py
@@ -51,4 +51,4 @@ async def search_leaks_by_domain(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
